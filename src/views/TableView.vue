60fa154da4b91dc1e4b3b7cbe874afa3caf7d65c
Thanush <thanushnarayanasamy@gmail.com>
2023-07-05 18:44:32 +0530

    First commit

---
+++ src/views/TableView.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul>
+        <li v-for="item in items" :key="item.id">{{ item.name }}  {{ item.age }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { name: "Thanush", age: 20, id: 1 },
+        { name: "Sri", age: 18, id: 2 },
+        { name: "Eswar", age: 20, id: 3 },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
